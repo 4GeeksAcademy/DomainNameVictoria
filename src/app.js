c924c59fb16d7748bfe7dde0924554ba97f73016
Victoria González Rodríguez <159072629+Vickygonrod@users.noreply.github.com>
2024-03-27 19:18:47 +0000

    Second commit dmg

--- src/app.js
+++ src/app.js
@@ -4,23 +4,34 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-import { ConcatenationScope } from "webpack";
 
-let pron = ["the", "our"];
-let adj = [
-  "great",
-  "big",
-  "one and only",
-  "amazing",
-  "incredible",
-  "spectacular"
-];
-let nom = ["backery", "bread house", "bread machine", "gluten cave"];
+window.onload = function() {
+  console.log("hola mundo");
 
-for (let i = 0; i < pron.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < nom.lenght; k++) {
-      console.log(`${pron[i]}$(adj[j])$(nom[k]).com`);
+  let pron = ["the", "our"];
+  let adj = [
+    "great",
+    "big",
+    "one and only",
+    "amazing",
+    "incredible",
+    "spectacular"
+  ];
+  let nom = ["backery", "breadhouse", "breadmachine", "glutencave"];
+
+  let textOf = '<ul class="list-group">';
+  textOf += '<li class="list-group-item bg-success text-light">Options</li>';
+
+  for (const iterator of pron) {
+    for (const item of adj) {
+      for (const element of nom) {
+        textOf += `
+          <li class="list-group-item">
+            ${iterator}${item}${element}.com
+          </li>`;
+      }
     }
   }
-}
+  textOf += "</ul>";
+  document.querySelector("#for").innerHTML = textOf;
+};
